d79c84188de3d424fa67c47a625fbffa692d9e65
jellyM0on <ylana.ong@gmail.com>
2026-05-10 21:55:34 +0800

    prototype: add vercel speed insights

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,6 +5,10 @@ import { AuthProvider } from "./context/AuthProvider";
 import "./styles/global.css";
 import "./styles/tokens.css";
 
+import { injectSpeedInsights } from '@vercel/speed-insights';
+
+injectSpeedInsights();
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <AuthProvider>
